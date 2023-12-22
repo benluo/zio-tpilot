4df19331481b00a8ac61ac70ff13fd4314159fda
dhuff <dallinhuff@gmail.com>
2023-12-22 12:00:00 -0700

    add HealthControllerSpec unit tests

---
+++ modules/server/src/test/scala/com/rockthejvm/reviewboard/http/controllers/HealthControllerSpec.scala
@@ -0,0 +1,35 @@
+package com.rockthejvm.reviewboard.http.controllers
+
+import com.rockthejvm.reviewboard.syntax.*
+
+import sttp.client3.*
+import sttp.client3.testing.SttpBackendStub
+import sttp.monad.MonadError
+import sttp.tapir.server.stub.TapirStubInterpreter
+import sttp.tapir.ztapir.RIOMonadError
+
+import zio.*
+import zio.test.*
+
+/** Specification for HealthController */
+object HealthControllerSpec extends ZIOSpecDefault:
+  override def spec: Spec[TestEnvironment with Scope, Any] =
+    suite("HealthControllerSpec")(
+      test("health check"):
+        val program = for
+          controller <- HealthController.makeZIO
+          endpoint <- ZIO.succeed(controller.health)
+          backendStub <- ZIO.succeed:
+            TapirStubInterpreter(SttpBackendStub(MonadError[Task]))
+              .whenServerEndpointRunLogic(endpoint)
+              .backend()
+          response <- basicRequest.get(uri"/health").send(backendStub)
+        yield response.body
+
+        program.assert("works")(_.toOption.contains("All good!"))
+    )
+  end spec
+
+  private given zioMonadError: MonadError[Task] =
+    new RIOMonadError[Any]
+end HealthControllerSpec
