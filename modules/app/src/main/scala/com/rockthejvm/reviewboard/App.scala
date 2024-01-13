ea2b903b87d62021d06649dcea510a9c410459b2
dhuff <dallinhuff@gmail.com>
2024-01-13 11:14:58 -0700

    setup frontend/app module
    - update existing and add frontend
      dependencies to build.sbt
    - create app module
      - add assets, css, index.html
      - create App.scala as main script src
    - update .gitignore to ignore npm and
      parcel files

---
+++ modules/app/src/main/scala/com/rockthejvm/reviewboard/App.scala
@@ -0,0 +1,15 @@
+package com.rockthejvm.reviewboard
+
+import com.raquo.laminar.api.L.{*, given}
+import org.scalajs.dom
+
+object App:
+  def main(args: Array[String]): Unit = {
+    val containerNode = dom.document.querySelector("#app")
+    render(containerNode,
+      div(
+        "Rock the JVM from Laminar!",
+        p("and also js")
+      )
+    )
+  }
