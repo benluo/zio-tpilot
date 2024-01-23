99ffdc325db54632de18eba160fee1072c875ac1
dhuff <dallinhuff@gmail.com>
2024-01-22 18:34:55 -0700

    add urlRegex to constants

--- modules/app/src/main/scala/com/rockthejvm/reviewboard/common/Constants.scala
+++ modules/app/src/main/scala/com/rockthejvm/reviewboard/common/Constants.scala
@@ -3,6 +3,7 @@ package com.rockthejvm.reviewboard.common
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
+/** Constants used throughout frontend web app */
 object Constants:
   @js.native
   @JSImport("/static/img/fiery-lava 128x128.png", JSImport.Default)
@@ -12,5 +13,10 @@ object Constants:
   @JSImport("/static/img/generic_company.png", JSImport.Default)
   val companyLogoPlaceholder: String = js.native
 
-  val emailRegex: String =
+  /** A regular expression for matching email addresses */
+  val emailRegex =
     """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"""
+
+  /** A regular expression for matching urls */
+  val urlRegex =
+    """^(https?):\/\/(([^:/?#]+)(?::(\d+))?)(\/[^?#]*)?(\?[^#]*)?(#.*)?"""
